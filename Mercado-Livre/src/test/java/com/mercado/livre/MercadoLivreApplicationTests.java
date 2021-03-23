5ebfd51a2e3d696430079bfe27cb80fa122d061d
redu <reduardo19942@gmail.com>
2021-03-23 13:21:49 -0300

    projeto iniciado

---
+++ Mercado-Livre/src/test/java/com/mercado/livre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercado.livre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
