f4a0d5d8670feacebfc991bf004b72f6b659e84a
redu <reduardo19942@gmail.com>
2021-03-23 13:27:55 -0300

    repository e request usuario adicionado

---
+++ Mercado-Livre/src/main/java/com/mercado/livre/usuario/UsuarioRequest.java
@@ -0,0 +1,32 @@
+package com.mercado.livre.usuario;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.validation.constraints.*;
+
+public class UsuarioRequest {
+    @NotNull
+    @NotBlank
+    private String login;
+    @NotNull
+    @NotBlank
+    @Min(6)
+    private String senha;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public Usuario converter() {
+        String novaSenha = gerarHash();
+        return new Usuario(login, novaSenha);
+    }
+
+    private String gerarHash() {
+        return new BCryptPasswordEncoder().encode(senha);
+    }
+}
