97a5fc3a0e48ac9e9c7528690ae10bade59e005e
redu <reduardo19942@gmail.com>
2021-03-24 14:29:59 -0300

    Security configure iniciado

--- Mercado-Livre/src/main/java/com/mercado/livre/MercadoLivreApplication.java
+++ Mercado-Livre/src/main/java/com/mercado/livre/MercadoLivreApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})//Evita exigir o login
+@SpringBootApplication
 public class MercadoLivreApplication {
 
 	public static void main(String[] args) {
