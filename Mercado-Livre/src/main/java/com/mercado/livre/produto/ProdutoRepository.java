670d750b1ab1928fd8bb2c967aaa46ea26ca6dd5
redu <reduardo19942@gmail.com>
2021-03-25 11:15:38 -0300

    repository de produto e caracteristicas

---
+++ Mercado-Livre/src/main/java/com/mercado/livre/produto/ProdutoRepository.java
@@ -0,0 +1,6 @@
+package com.mercado.livre.produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
---
+++ Mercado-Livre/src/main/java/com/mercado/livre/produto/caracteristica/CaracteristicaRepository.java
@@ -0,0 +1,6 @@
+package com.mercado.livre.produto.caracteristica;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CaracteristicaRepository extends JpaRepository<Caracteristica, Long> {
+}
